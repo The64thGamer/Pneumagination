14cea9e457a2e461dc08bba8cf50979d7909ce03
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-08-13 14:45:43 -0500

    Items can be placed and hotbar cycled

--- Scripts/Player/Inventory.cs
+++ Scripts/Player/Inventory.cs
@@ -8,6 +8,7 @@ public partial class Inventory : Control
 	InventoryItem offHandItem;
 	[Export] VBoxContainer uiInvArray;
 	[Export] Control hotbar;
+	[Export] Label hotbarTabText;
 	Godot.Collections.Array<Node> hotbarBoxes;
 
 	bool canInput = true;
@@ -16,6 +17,7 @@ public partial class Inventory : Control
 	bool nodeScreen = false;
 	Input.MouseModeEnum oldMouse;
 	int hotbarIndex;
+	int inventoryIndexOfHotbar;
 	InputManager inputManager;
 
     void OnToggleMenu(bool menu)
@@ -43,13 +45,40 @@ public partial class Inventory : Control
 		inventory.Add(new InventoryItem()
 		{
 			name = "Lemon",
-			count = 1,
+			count = 5,
+		});
+		for (int i = 0; i < 20; i++)
+		{
+			inventory.Add(null);
+		}
+		inventory.Add(new InventoryItem()
+		{
+			name = "Lemon",
+			count = 4,
 		});
 		RefreshInventoryLayout();
 	}
 
 	public override void _Process(double delta)
 	{
+		if (Input.IsActionJustPressed("Hotbar Down") && Input.IsActionPressed("Shift"))
+		{
+			inventoryIndexOfHotbar = inventoryIndexOfHotbar-1;
+			if(inventoryIndexOfHotbar < 0)
+			{
+				inventoryIndexOfHotbar = Mathf.CeilToInt(inventory.Count / 9.0f);
+			}
+			RefreshInventoryLayout();
+		}
+		if (Input.IsActionJustPressed("Hotbar Down"))
+		{
+			inventoryIndexOfHotbar = inventoryIndexOfHotbar+1;
+			if(inventoryIndexOfHotbar >= Mathf.Ceil(inventory.Count / 9.0f))
+			{
+				inventoryIndexOfHotbar = 0;
+			}
+			RefreshInventoryLayout();
+		}
 		if (Input.IsActionJustPressed("Scroll Up"))
 		{
 			hotbarIndex = hotbarIndex-1;
@@ -68,6 +97,37 @@ public partial class Inventory : Control
 			}
 			RefreshInventoryLayout();
 		}
+		if (Input.IsActionJustPressed("Action"))
+		{
+			PlaceItem();
+		}
+	}
+
+	public void PlaceItem()
+	{
+		Camera3D camera3D = GetViewport().GetCamera3D();
+		PhysicsDirectSpaceState3D spaceState = camera3D.GetWorld3D().DirectSpaceState;
+		PhysicsRayQueryParameters3D query = PhysicsRayQueryParameters3D.Create(camera3D.GlobalPosition, camera3D.GlobalPosition + (-camera3D.GlobalTransform.Basis.Z * PlayerMovement.playerReach));
+		query.CollisionMask = 0b00000000_00000000_00000000_00000100; //Brushes
+		Godot.Collections.Dictionary result = spaceState.IntersectRay(query);
+		if (result.Count > 0)
+		{
+			int hotbarSelection = (inventoryIndexOfHotbar*9)+hotbarIndex;
+			if(hotbarSelection < inventory.Count && inventory[hotbarSelection] != null)
+			{
+				Node3D newNode = GD.Load<PackedScene>("res://Prefabs/Items/"+ inventory[hotbarSelection].name + ".tscn").Instantiate() as Node3D;
+				newNode.Position = (Vector3)result["position"];
+				GetTree().Root.AddChild(newNode);
+				inventory[hotbarSelection].count -= 1;
+				if(inventory[hotbarSelection].count == 0)
+				{
+					inventory[hotbarSelection] = null;
+				}
+							RefreshInventoryLayout();
+
+			}
+
+		}
 	}
 
 	//Redo this to not delete bars and instead reuse them
@@ -80,7 +140,7 @@ public partial class Inventory : Control
 
 		for (int i = 0; i < Mathf.CeilToInt(inventory.Count / 9.0f)+1; i++)
 		{
-			Control bar = (ResourceLoader.Load<PackedScene>("res://Prefabs/UI/Inventory Item Bar.tscn").Instantiate()) as Control;
+			Control bar = ResourceLoader.Load<PackedScene>("res://Prefabs/UI/Inventory Item Bar.tscn").Instantiate() as Control;
 
 			Godot.Collections.Array<Node> childs = bar.GetChild(0).GetChildren();
 			for (int e = 0; e < childs.Count; e++)
@@ -92,8 +152,10 @@ public partial class Inventory : Control
 
 		for (int e = 0; e < hotbarBoxes.Count; e++)
 		{
-			RefreshItemBox(hotbarBoxes[e] as Control, (0*9)+e, hotbarIndex == e);
+			RefreshItemBox(hotbarBoxes[e] as Control, (inventoryIndexOfHotbar*9)+e, hotbarIndex == e);
 		}
+
+		hotbarTabText.Text = ((char)('A' + inventoryIndexOfHotbar)).ToString();
 	}
 
 	void RefreshItemBox(Control control, int index, bool highlighted)
